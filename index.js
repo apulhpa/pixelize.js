aa417a1d37fd5e0a46dd9bd5740fd301ebe0ef2f
Pixelize <apulhpa@gmail.com>
2023-11-12 18:21:14 +0000

    Added Custom Status Feature

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-const axios = require('axios')
+const axios = require('axios');
 
 class Pixelize {
     constructor(token) {
         this.token = token;
         this.apiBase = 'https://discord.com/api/v10';
+    }
+
+    async setCustomStatus(statusText, statusType = 'PLAYING', statusState = 'online') {
+        try {
+            await axios.patch(
+                `${this.apiBase}/users/@me/settings`,
+                {
+                    custom_status: {
+                        text: statusText,
+                    },
+                },
+                {
+                    headers: {
+                        Authorization: `Bot ${this.token}`,
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
+            await this.client.user.setPresence({
+                activity: {
+                    name: statusText,
+                    type: statusType,
+                },
+                status: statusState,
+            });
+            console.log(`Custom status set: ${statusText}`);
+        } catch (error) {
+            console.error('Error setting custom status:', error.response.data);
+            throw error;
+        }
     }
 
     async sendMessage(channelId, content) {
@@ -12,16 +42,16 @@ class Pixelize {
                 `${this.apiBase}/channels/${channelId}/messages`,
                 { content },
                 {
-                  headers: {
-                    Authorization: `Bot ${this.token}`,
-                    'Content-Type': 'application/json',
-                  },
+                    headers: {
+                        Authorization: `Bot ${this.token}`,
+                        'Content-Type': 'application/json',
+                    },
                 }
-              );
-              return response.data;
-            } catch (error) {
-              console.error('Error sending message:', error.response.data);
-              throw error;
+            );
+            return response.data;
+        } catch (error) {
+            console.error('Error sending message:', error.response.data);
+            throw error;
         }
     }
 }
